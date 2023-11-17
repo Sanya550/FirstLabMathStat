7024c432a2d8a69301ff2e9774586909a9dbaf59
Sanya550 <pivovarsasa9@gmail.com>
2023-11-17 20:04:45 +0200

    matrix riznomanitia(2)

--- src/main/java/com/example/idealjavafx/logicHelper/FactorHelper.java
+++ src/main/java/com/example/idealjavafx/logicHelper/FactorHelper.java
@@ -3,10 +3,8 @@ package com.example.idealjavafx.logicHelper;
 import com.example.idealjavafx.MainFunction;
 import org.apache.commons.math3.linear.RealVector;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class FactorHelper {
     private static final double eps = 0.001;
@@ -202,34 +200,59 @@ public class FactorHelper {
         var wMatrixFromKorilation = findWMatrix(korilationMatrix, 1);
 
         var firstMap = getActualRHAndWFirst(korilationMatrix, listNotSorted, wMatrixFromKorilation.length);
-        int wValue = firstMap.entrySet().iterator().next().getKey().get(0).intValue();
-        var matrix = firstMap.entrySet().iterator().next().getValue();
-        var newVlArray = getNewVlMatrixWithHkOnDiagonals(matrix, wValue);
-        var secondMap = getActualRHAndWSecond(newVlArray, wMatrixFromKorilation.length);
+        var secondMap = getTempMap(firstMap);
         while (isConditionAccepted(firstMap, secondMap)) {
             firstMap = secondMap;
-            wValue = firstMap.entrySet().iterator().next().getKey().get(0).intValue();
-            matrix = firstMap.entrySet().iterator().next().getValue();
-            newVlArray = getNewVlMatrixWithHkOnDiagonals(matrix, wValue);
-            secondMap = getActualRHAndWSecond(newVlArray, wMatrixFromKorilation.length);
+            secondMap = getTempMap(firstMap);
         }
 
 
-        var matrixResult = secondMap.entrySet().iterator().next().getValue();
+        var matrixResult = MainFunction.transposeMatrix(MainFunction.convectListOfRealVectorsToRwoDimensionArray(MainFunction.getVlasniiVectors(secondMap.entrySet().iterator().next().getValue())));
         var quantityOfFactors = secondMap.entrySet().iterator().next().getKey().get(0).intValue();
+        var vl1 = MainFunction.getVlasniiValues(secondMap.entrySet().iterator().next().getValue());
+        List<Double> vl = vl1.stream()
+                .map(v -> Math.abs(v))
+                .collect(Collectors.toList());
+        var newVl = new ArrayList<>(vl);
+        newVl.sort(Comparator.reverseOrder());
+
+        var sumOfVl = newVl.stream().mapToDouble(v -> v).sum();
+        var potentialNewQuantityOfFactors = newVl.stream().filter(v -> v / sumOfVl > 0.1).collect(Collectors.toList()).size();
+        if (potentialNewQuantityOfFactors > quantityOfFactors) {
+            quantityOfFactors = potentialNewQuantityOfFactors;
+        }
+        if (quantityOfFactors == matrixResult.length) {
+            quantityOfFactors--;
+        }
+
+        var newResultMatrix = new double[matrixResult.length][matrixResult.length];
+        for (int i = 0; i < vl.size(); i++) {
+            var val = -1;
+            var temp = newVl.get(i);
+            for (int j = 0; j < vl.size(); j++) {
+                if (temp.equals(vl.get(j))) {
+                    val = j;
+                    break;
+                }
+            }
+            for (int j = 0; j < matrixResult.length; j++) {
+                newResultMatrix[j][i] = matrixResult[j][val];
+            }
+        }
+
         String result = "  ";
         for (int i = 0; i < quantityOfFactors; i++) {
             result += String.format("      F%d  ", i + 1);
         }
-        result += "    h_k  \n";
+        result += "  h_k  \n";
         int r = 1;
-        for (int i = 0; i < matrixResult.length; i++) {
+        for (int i = 0; i < newResultMatrix.length; i++) {
             var sum = 0d;
             for (int j = 0; j < quantityOfFactors; j++) {
                 if (j == 0) {
                     result += String.format("X%d  ", r);
                 }
-                var val = matrixResult[i][j];
+                var val = newResultMatrix[i][j];
                 if (val > 0) {
                     result += " ";
                 }
@@ -239,23 +262,63 @@ public class FactorHelper {
             result += String.format("%.3f\n", sum);
             r++;
         }
+
+        result += "напр% ";
+        double[] dataV = new double[quantityOfFactors];
+
+        var sumVl = 0d;
+        for (int i = 0; i < newVl.size(); i++) {
+            sumVl += Math.abs(newVl.get(i));
+        }
+
+        for (int i = 0; i < quantityOfFactors; i++) {
+            dataV[i] = Math.abs(newVl.get(i) / sumVl);
+        }
+
+        for (int i = 0; i < dataV.length; i++) {
+            result += String.format("  %.2f", dataV[i]);
+        }
+
+        result += "\nнакоп% ";
+        int st1 = 1;
+        for (int i = 0; i < dataV.length; i++) {
+            var temp = 0d;
+            for (int j = 0; j < st1; j++) {
+                temp += dataV[j];
+            }
+            result += String.format("  %.2f", temp);
+            st1++;
+        }
         return result;
     }
 
-    private double[][] getNewVlMatrixWithHkOnDiagonals(double[][] matrix, int wValue) {
-        var vlMatrixExpandedWithOldHk = MainFunction.convectListOfRealVectorsToRwoDimensionArray(MainFunction.getVlasniiVectors(matrix));
-        double[] hk = getHkFromMatrix(wValue, vlMatrixExpandedWithOldHk);
-        var vlMatrixExpandedWithNeHkOnDiagonal = new double[vlMatrixExpandedWithOldHk.length][vlMatrixExpandedWithOldHk.length];
-        for (int i = 0; i < vlMatrixExpandedWithOldHk.length; i++) {
-            for (int j = 0; j < vlMatrixExpandedWithOldHk.length; j++) {
-                if (i != j) {
-                    vlMatrixExpandedWithNeHkOnDiagonal[i][j] = vlMatrixExpandedWithOldHk[i][j];
+    private HashMap<List<Double>, double[][]> getTempMap(HashMap<List<Double>, double[][]> firstMap) {
+        var matrix1 = firstMap.entrySet().iterator().next().getValue();
+        var matrix = MainFunction.convectListOfRealVectorsToRwoDimensionArray(MainFunction.getVlasniiVectors(matrix1));
+        var wValue = firstMap.entrySet().iterator().next().getKey().get(0);
+        double[] newHk = new double[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            double tSum = 0d;
+            for (int j = 0; j < wValue; j++) {
+                tSum += Math.pow(matrix[i][j], 2);
+            }
+            newHk[i] = tSum;
+        }
+
+        var newMatrix = new double[matrix1.length][matrix1.length];
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix1.length; j++) {
+                if (i == j) {
+                    newMatrix[i][j] = newHk[i];
                 } else {
-                    vlMatrixExpandedWithNeHkOnDiagonal[i][j] = hk[i];
+                    newMatrix[i][j] = matrix1[i][j];
                 }
             }
         }
-        return vlMatrixExpandedWithNeHkOnDiagonal;
+        var newF = findFOfRzal(newMatrix);
+        var secondMap = new HashMap<List<Double>, double[][]>();
+        secondMap.put(List.of(wValue, newF), newMatrix);
+        return secondMap;
     }
 
     private double[] getHkFromMatrix(int wValue, double[][] matrix) {
@@ -305,48 +368,17 @@ public class FactorHelper {
         return map;
     }
 
-
-    private HashMap<List<Double>, double[][]> getActualRHAndWSecond(double[][] matrix, int wInitialLength) {
-        var mapOfRhAndF = new HashMap<Double, double[][]>();
-        var tempValueForMin = findFOfRzal(findRHMatrix(matrix, hByMethodMaxKorilation(matrix)));
-        var rHMatrixMaxKorilation = findRHMatrix(matrix, hByMethodMaxKorilation(matrix));
-        var rHMatrixTriad = findRHMatrix(matrix, hByMethodTriad(matrix));
-        var rHMatrixAverage = findRHMatrix(matrix, hByMethodOfAverage(matrix));
-        var rHMatrixZentroid = findRHMatrix(matrix, hByMethodOfZentroid(matrix));
-        var rHMatrixAveroid = findRHMatrix(matrix, hByMethodOfAveroid(matrix));
-        mapOfRhAndF.put(findFOfRzal(rHMatrixMaxKorilation), rHMatrixMaxKorilation);
-        mapOfRhAndF.put(findFOfRzal(rHMatrixTriad), rHMatrixTriad);
-        mapOfRhAndF.put(findFOfRzal(rHMatrixAverage), rHMatrixAverage);
-        mapOfRhAndF.put(findFOfRzal(rHMatrixZentroid), rHMatrixZentroid);
-        mapOfRhAndF.put(findFOfRzal(rHMatrixAveroid), rHMatrixAveroid);
-
-        for (var el : mapOfRhAndF.entrySet()) {
-            if (el.getKey() < tempValueForMin) {
-                tempValueForMin = el.getKey();
-            }
-        }
-        var actualRHMatrix = mapOfRhAndF.get(tempValueForMin);
-        var averageOfVlasniNumbers = MainFunction.getVlasniiValues(actualRHMatrix).stream().mapToDouble(a -> a).average().orElseThrow();
-        var wMatrixFromRh = findWMatrix(actualRHMatrix, averageOfVlasniNumbers);
-        int higherW = 0;
-        if (wInitialLength > wMatrixFromRh.length) {
-            higherW = wInitialLength;
-        } else {
-            higherW = wMatrixFromRh.length;
-        }
-        var map = new HashMap<List<Double>, double[][]>();
-        map.put(Arrays.asList((double) higherW, tempValueForMin), actualRHMatrix);//w value(first) and f value(second)
-        return map;
-    }
-
     private boolean isConditionAccepted(HashMap<List<Double>, double[][]> oldMap, HashMap<List<Double>, double[][]> newMap) {
         //check f(i+1)<f(i)
         if (oldMap.entrySet().iterator().next().getKey().get(1) < newMap.entrySet().iterator().next().getKey().get(1)) {
             return false;
         }
 
-        var matrixAOldArray = oldMap.entrySet().iterator().next().getValue();
-        var matrixANewArray = newMap.entrySet().iterator().next().getValue();
+//        var matrixAOldArray = oldMap.entrySet().iterator().next().getValue();
+//        var matrixANewArray = newMap.entrySet().iterator().next().getValue();
+
+        var matrixAOldArray = MainFunction.convectListOfRealVectorsToRwoDimensionArray(MainFunction.getVlasniiVectors(oldMap.entrySet().iterator().next().getValue()));
+        var matrixANewArray = MainFunction.convectListOfRealVectorsToRwoDimensionArray(MainFunction.getVlasniiVectors(newMap.entrySet().iterator().next().getValue()));
 
         var tempSum = 0d;
         for (int i = 0; i < matrixAOldArray.length; i++) {
@@ -365,14 +397,6 @@ public class FactorHelper {
         }
         return true;
     }
-
-//    var vl = MainFunction.getVlasniiVectors(matrix);
-    //обрезацию матрицу vl
-    //нахожу аш
-    // ставлю на диагонали RH
-    // на опять f
-    //повторяю
-    // res: обрезаная матрица vl и
 
     private double[][] getCentruvanDcMatrix(List<List<Double>> listNotSorted) {
         List<List<Double>> list1 = new ArrayList<>();
